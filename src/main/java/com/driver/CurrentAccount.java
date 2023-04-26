bdee7d7020e8d7c7343cff74349055ef3a5fc8dd
Honey Chauhan <hc924508@gmail.com>
2023-04-26 21:33:08 +0530

    first submission

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,25 @@
 package com.driver;
 
+import java.util.*;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
+    public final static int minBalance = 5000;
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
+        super(name,balance,minBalance);
+        if(balance<minBalance)throw new Exception("Insufficient Balance");
+        this.tradeLicenseId = tradeLicenseId;
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
+    }
+
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -14,6 +28,40 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        int size = tradeLicenseId.length();
+        HashMap<Character,Integer> hm = new HashMap<>();
+        getFrequencyMap(size, hm);
+
+        if(Collections.max(hm.values()) > (size +1)/2){
+            throw new Exception("Valid License can not be generated");
+        }
+
+        while(!isValidTradeLicenseId()){
+            List list = Arrays.asList(tradeLicenseId.toCharArray());
+            Collections.shuffle(list);
+            this.tradeLicenseId = list.toString();
+        }
+        return;
+    }
+
+    private void getFrequencyMap(int size, HashMap<Character, Integer> hm) {
+        for(int i = 0; i< size; i++){
+            char ch = tradeLicenseId.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch,0)+1);
+        }
+    }
+
+    private boolean isValidTradeLicenseId() {
+        int size = tradeLicenseId.length();
+        for(int i = 0, j = 1; i< size && j< size; i++,j++){
+            if(tradeLicenseId.charAt(i)==tradeLicenseId.charAt(j)){
+                break;
+            }
+            if(j== size -1){
+                return true;
+            }//tradeLicenseID is valid
+        }
+        return false;
     }
 
 }
--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -3,9 +3,12 @@ package com.driver;
 public class StudentAccount extends BankAccount{
 
     String  institutionName;
+    final static double minBalance = 0;
 
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
+        super(name,balance,minBalance);
+        this.institutionName = institutionName;
 
     }
 
